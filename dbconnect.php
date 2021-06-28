a4f3ada1eb6c440ca22e330e990025f6b413c1c5
Randisyustico <52877998+Healer-ID@users.noreply.github.com>
2021-06-28 21:29:25 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","inventory");
+$conn = mysqli_connect("localhost","root","","si-perpustakaan");
 
 ?>
